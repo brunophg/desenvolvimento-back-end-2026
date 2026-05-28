9a663e07130e2364de831c0dc60c6314e2f90508
brunophg <brunophgjf@gmail.com>
2026-05-28 12:08:58 -0300

    fix: InventarioRequest & ItemRequest

--- T3/src/main/java/org/dto/inventario/InventarioRequest.java
+++ T3/src/main/java/org/dto/inventario/InventarioRequest.java
@@ -1,13 +1,18 @@
 package org.dto.inventario;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.model.Inventario;
 import org.model.Item;
 import org.model.Jogador;
 
 public record InventarioRequest(
+        @Schema(description = "Identificador do inventario. Opcional na criacao.", example = "1")
         Long id,
+        @Schema(description = "Quantidade de itens guardados", example = "5")
         Integer quantidade,
+        @Schema(description = "ID do Jogador dono do inventário", example = "4001")
         Long idJogador,
+        @Schema(description = "ID do Item sendo guardado", example = "4001")
         Long idItem
 ) {
     public Inventario toEntity() {
